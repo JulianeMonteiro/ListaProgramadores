0cf77bfd72a8dc793e7ed6c49ff9a8d871648332
JulianeMonteiro <51388071+JulianeMonteiro@users.noreply.github.com>
2021-07-06 18:53:10 -0300

    Initial commit

---
+++ app/src/main/java/com/juliane/listaprogramadores/Senioridade.kt
@@ -0,0 +1,7 @@
+package com.juliane.listaprogramadores
+
+enum class Senioridade(val nome: String) {
+    JUNIOR("Junior"),
+    PLENO("Pleno"),
+    SENIOR("Senior")
+}
---
+++ app/src/main/java/com/juliane/listaprogramadores/Stack.kt
@@ -0,0 +1,7 @@
+package com.juliane.listaprogramadores
+
+enum class Stack(val nome: String) {
+    FRONTEND("Frontend"),
+    BACKEND("Backend"),
+    FULLSTACK("Fullstack")
+}
